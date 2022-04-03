7a95717ba2cf5af73aafbdf85784e936db47e7dc
kay-ogunbo <o.ogunbowal@gmail.com>
2022-04-02 21:55:14 -0500

    Initial commit

---
+++ script.js
@@ -0,0 +1,7 @@
+const body = document.querySelector('body'),
+      modeToggle = body.querySelector(".mode-toggle");
+
+modeToggle.addEventListener("click", () => {
+    body.classList.toggle("dark");
+})
+
